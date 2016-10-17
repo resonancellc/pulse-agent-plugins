5e102d49034da14885444d87dc679536e3d719e7
Jean-françois kneib <jean-francois.kneib@siveo.net>
2016-10-17 16:22:00 +0200

    plugin guacamole write guacamole configuration on relay server

--- pulse_agent_plugins/relay/plugin_guacamoleconf.py
+++ pulse_agent_plugins/relay/plugin_guacamoleconf.py
@@ -6,67 +6,85 @@ import sys, os, platform
 from  lib.utils import pulginprocess
 from lxml import etree
 import MySQLdb
+import traceback
+plugin={"VERSION": "1.0", "NAME" :"guacamoleconf", "TYPE":"relayserver"}
 
-plugin={"VERSION": "1.0", "NAME" :"relayserver", "TYPE":"relayserver"}
-# ce plugin install la configuration de guacamole sur server relais.
-# When using guacamole-auth-noauth, you have to logout of Guacamole (or clear cookies, etc.)
-# for changes to noauth-config.xml to take effect.
-def xmlgeneration(nameconfig, protocol, hostname, port):
-    """
-        #<config name="myconfig" protocol="rdp">
-            #<param name="hostname" value="rdp-server" />
-            #<param name="port" value="3389" />
-        #</config>
-    """
-    CONFIG =  etree.Element('config')
-    #CONFIG.text = 
-    CONFIG.set('name', nameconfig)
-    CONFIG.set('protocol', protocol)
-    HOSTNAME = etree.SubElement(CONFIG,'param')
-    #QUERY.text = 'INVENTORY'
-    HOSTNAME.set('name', 'hostname')
-    HOSTNAME.set('value', hostname)
-    PORT = etree.SubElement(CONFIG,'param')
-    PORT.set('name', 'port')
-    PORT.set('value', str(port))
-    return etree.tostring(CONFIG, pretty_print=True)
 
-#self.callInstallConfGuacamole(self, jidrs, {'hostname' : data['information']['info']['hostname'],
-                                                                        #'machine_ip' : data['xmppip'],
-                                                                        #'uuid' : str(computer.id) })
+def insertprotocole(protocole, hostname):
+    return """INSERT INTO guacamole_connection (connection_name, protocol) VALUES ( '%s_%s', '%s');"""%(protocole.upper(), hostname, protocole.lower())
+
+def deleteprotocole(protocole, hostname):
+    return """DELETE FROM `guacamole_connection` WHERE connection_name = '%s_%s';"""%(protocole.upper(),hostname)
+
+def insertparameter(id, parameter, value):
+    return """INSERT INTO guacamole_connection_parameter (connection_id, parameter_name, parameter_value) VALUES (%s, '%s', '%s');"""%(id,parameter,value)
+
 @pulginprocess
 def action( objetxmpp, action, sessionid, data, message, dataerreur,result):
     
-    #try :
-        #from  fichierdecomf import fileconf
-    #except:
-        #dataerreur['ret']=255
-        #dataerreur['data']['msg'] = "ERROR : relayserver verify execution from relayserver"
-        #raise 
-    if sys.platform.startswith('linux'):
-        # write config to /etc/guacamole/noauth-config.xml
-        #print "%s"% json.dumps(data[0], indent=4, sort_keys=True)
-        xml=""
-        protocolewindows = {'rdp': 3389,'ssh':22}
-        protocolelinux   = {'VNC': 5900, 'ssh':22}
-        for donnees in data:
-            if donnees['os']=="Linux":
-                for prot,po in protocolelinux.iteritems():
-                    xml = xml + xmlgeneration(donnees['hostname'], prot, donnees['xmppip'], po)
-        try:
-            with open("/etc/guacamole/noauth-config.xml", 'w') as file:
-                file.write('<configs>\n')
-                file.write(xml)
-                file.write('</configs>\n')
-        except IOError:
-            dataerreur['data']['msg'] = "ERROR : write file /etc/guacamole/noauth-config.xml"
-            dataerreur['ret']=255
-            raise
-        result['data']['msg'] = "install configuration guacamole"
-        result['data']['info'] = data
-        result['base64'] = False
-    else:
-        #mac os a completer
-        dataerreur['data']['msg'] = "ERROR : relayserver  must is linux"   
-        dataerreur['ret']=255
+    # print objetxmpp.config
+    # Open database connection
+    try:
+        db = MySQLdb.connect(   host=objetxmpp.config.guacamole_dbhost,
+                                user=objetxmpp.config.guacamole_dbuser,
+                                passwd=objetxmpp.config.guacamole_dbpasswd,
+                                db=objetxmpp.config.guacamole_dbname )
+    except Exception as e:
+        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
+        traceback.print_exc(file=sys.stdout)
         raise
+    cursor=db.cursor()
+    result['data']['uuid'] = data['uuid']
+    result['data']['connection'] = {}
+
+    protos = ['rdp','ssh','vnc']
+
+    try:
+        #delete connection
+        for proto in protos:
+            cursor.execute(deleteprotocole(proto, data['hostname']))
+            db.commit()
+        #create connection
+        for proto in protos:
+            result['data']['connection'][proto.upper()] = -1
+            cursor.execute(insertprotocole(proto, data['hostname']))
+            db.commit()
+            result['data']['connection'][proto.upper()] = cursor.lastrowid
+    except MySQLdb.Error, e:
+        db.close()
+        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
+        traceback.print_exc(file=sys.stdout)
+        raise
+    except Exception, e:
+        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
+        traceback.print_exc(file=sys.stdout)
+        db.close()
+        raise
+    ###################################
+    ##configure parameters
+    ###################################
+    try:
+        for proto in protos:
+            if proto == 'rdp':
+                port = '3389'
+            elif proto == 'ssh':
+                port = '22'
+            else:
+                port = '5901'
+            cursor.execute(insertparameter(result['data']['connection'][proto.upper()], 'hostname', data['machine_ip']))
+            db.commit()
+            cursor.execute(insertparameter(result['data']['connection'][proto.upper()], 'port', port))
+            db.commit()
+            cursor.execute(insertparameter(result['data']['connection'][proto.upper()], 'color-depth', '24'))
+            db.commit()
+    except MySQLdb.Error, e:
+        db.close()
+        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
+        traceback.print_exc(file=sys.stdout)
+        raise
+    except Exception, e:
+        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
+        traceback.print_exc(file=sys.stdout)
+        db.close()
+        raise
+    db.close()
