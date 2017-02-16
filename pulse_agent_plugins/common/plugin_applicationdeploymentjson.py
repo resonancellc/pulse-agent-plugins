43416734c508aac6c93913be338bc74c1af4dae2
jfkneib <jean-francois.kneib@siveo.net>
2017-02-16 09:18:57 +0100

    chang plugin deloy

--- pulse_agent_plugins/common/plugin_applicationdeploymentjson.py
+++ pulse_agent_plugins/common/plugin_applicationdeploymentjson.py
@@ -26,7 +26,7 @@ from lib.grafcetdeploy import grafcet
 import traceback
 import pprint
 import logging
-
+import pycurl
 
 from lib.utils import shellcommandtimeout
 
@@ -50,59 +50,57 @@ Plugins for deploiment application
 
 
 
-
-
 def cleandescriptor(datasend):
     sequence= {}
     if sys.platform.startswith('linux'):
         typeos="Linux"
         try:
-            del datasend['data']['descriptor']['win']['sequence']
+            del datasend['descriptor']['win']['sequence']
         except KeyError:
             pass
         try:
-            del datasend['data']['descriptor']['Macos']['sequence']
+            del datasend['descriptor']['Macos']['sequence']
         except KeyError:
             pass
         try:
-            datasend['data']['descriptor']['sequence'] = datasend['data']['descriptor']['linux']['sequence']
-            #del datasend['data']['descriptor']['linux']['sequence']
-            del datasend['data']['descriptor']['linux']
+            datasend['descriptor']['sequence'] = datasend['descriptor']['linux']['sequence']
+            #del datasend['descriptor']['linux']['sequence']
+            del datasend['descriptor']['linux']
         except:
             pass
     elif sys.platform.startswith('win'):
         typeos="Win"
         try:
-            del datasend['data']['descriptor']['linux']['sequence']
+            del datasend['descriptor']['linux']['sequence']
         except KeyError:
             pass
         try:
-            del datasend['data']['descriptor']['Macos']['sequence']
+            del datasend['descriptor']['Macos']['sequence']
         except KeyError:
             pass
         try:
-            datasend['data']['descriptor']['sequence'] = datasend['data']['descriptor']['win']['sequence']
-            #del datasend['data']['descriptor']['win']['sequence']
-            del datasend['data']['descriptor']['win']
+            datasend['descriptor']['sequence'] = datasend['descriptor']['win']['sequence']
+            #del datasend['descriptor']['win']['sequence']
+            del datasend['descriptor']['win']
         except:
             pass
     elif sys.platform.startswith('darwin'):
         typeos="Macos"
         try:
-            del datasend['data']['descriptor']['linux']['sequence']
+            del datasend['descriptor']['linux']['sequence']
         except KeyError:
             pass
         try:
-            del datasend['data']['descriptor']['win']['sequence']
+            del datasend['descriptor']['win']['sequence']
         except KeyError:
             pass
         try:
-            datasend['data']['descriptor']['sequence'] = datasend['data']['descriptor']['Macos']['sequence']
-            #del datasend['data']['descriptor']['Macos']['sequence']
-            del datasend['data']['descriptor']['Macos']
+            datasend['descriptor']['sequence'] = datasend['descriptor']['Macos']['sequence']
+            #del datasend['descriptor']['Macos']['sequence']
+            del datasend['descriptor']['Macos']
         except:
             pass
-    datasend['data']['typeos']=typeos
+    datasend['typeos']=sys.platform
     return datasend
 
 def keyssh(name="id_rsa.pub"):
@@ -188,6 +186,40 @@ def checkosindescriptor(descriptor):
         return False
 
 
+def curlgetdownloadfile( destfile, urlfile, insecure = True):
+    # As long as the file is opened in binary mode, both Python 2 and Python 3
+    # can write response body to it without decoding.
+    with open(destfile, 'wb') as f:
+        c = pycurl.Curl()
+        c.setopt(c.URL, urlfile)
+        c.setopt(c.WRITEDATA, f)
+        if insecure :
+            # option equivalent a friser de --insecure
+            c.setopt(pycurl.SSL_VERIFYPEER, 0)   
+            c.setopt(pycurl.SSL_VERIFYHOST, 0)
+        c.perform()
+        c.close()
+
+def recuperefile(datasend, objectxmpp):
+    if not os.path.isdir(datasend['data']['pathpackageonmachine']):
+        os.makedirs(datasend['data']['pathpackageonmachine'], mode=0777)
+    uuidpackage = datasend['data']['path'].split('/')[-1]
+    curlurlbase = "https://%s:9990/mirror1_files/%s/"%(datasend['data']['iprelay'], uuidpackage )
+    #curl -O -k  https://192.168.56.2:9990/mirror1_files/0be145fa-973c-11e4-8dc5-0800275891ef/7z920.exe
+    for filepackage in datasend['data']['packagefile']:
+        if datasend['data']['methodetransfert'] == "curl":
+            src  = os.path.join(datasend['data']['path'], filepackage)
+            dest = os.path.join(datasend['data']['pathpackageonmachine'], filepackage)
+            urlfile= curlurlbase + filepackage
+            print "curl file dest %s  url  %s"%(dest,urlfile)
+            curlgetdownloadfile( dest, urlfile )
+            objectxmpp.logtopulse('download from %s file : %s'%( datasend['data']['jidrelay'], filepackage ) ,
+                                       type='deploy',
+                                       sessionname = datasend['sessionid'] ,
+                                       priority = -1,
+                                       who = objectxmpp.boundjid.bare)
+
+
 def action( objectxmpp, action, sessionid, data, message, dataerreur):
     logging.getLogger().debug("RECV data message")
     #if not 'stepcurrent' in data:
@@ -201,108 +233,34 @@ def action( objectxmpp, action, sessionid, data, message, dataerreur):
                     'base64' : False
                 }
 
- 
-    if objectxmpp.config.agenttype == "relayserver":
-        logging.getLogger().debug("###############RELAY SERVERS##################")
-        logging.getLogger().debug("##############deploy %s on %s##############"%(data['name'],data['jidmachine'] ))
-        logging.getLogger().debug("#############################################")
-        # creation session relay server if not exist
-        # demande deploy sur machine
-        print "session est", sessionid
+    logging.getLogger().debug("#################MACHINE#####################")
+    logging.getLogger().debug("##############deploy %s on %s##############"%(data['name'],data['jidmachine'] ))
+    logging.getLogger().debug("#############################################")
+    print "recois message"
+    print json.dumps(data, indent=4, sort_keys=True)
+    if not 'stepcurrent' in datasend['data']:
+        cleandescriptor(data)
+        datasend = {
+                        'action': action,
+                        'sessionid': sessionid,
+                        'data' : data,
+                        'ret' : 0,
+                        'base64' : False
+                    }
+
+        datasend['data']['pathpackageonmachine'] = os.path.join( managepackage.packagedir(),data['path'].split('/')[-1])
+        print json.dumps(datasend, indent=4, sort_keys=True)
+        if data['methodetransfert'] == "curl" and data['transfert'] :
+            recuperefile(datasend, objectxmpp )
+        datasend['data']['stepcurrent'] = 0 #step initial
         if not objectxmpp.session.isexist(sessionid):
-            #test package exist on relayserver
-            print "**************session non existe"
-            if managepackage.getpathpackagename(data['name']) is None:
-                logging.getLogger().info("packages %s missing on relayserver %s"%(data['name'],data['jidrelay']))
-                print "deploye error"
-                return
-            try:
-                #embarquement du descriptaur de deployement
-                datasend['data']['descriptor'] =  managepackage.getdescriptorpackagename(data['name'])
-                #creation session
-                objectxmpp.session.createsessiondatainfo(sessionid,  datasession = data, timevalid = 10)
-                logging.getLogger().debug("###############RELAY SERVER################## add clef au message")
-                datasend['data']['keyssh']=keyssh()
-                logging.getLogger().debug("send data to %s\n %s"%(data['jidmachine'] ,json.dumps(datasend, indent=4, sort_keys=True)))
-
-                objectxmpp.send_message(    mto=data['jidmachine'],
-                                            mbody=json.dumps(datasend),
-                                            mtype='chat')
-                print "attend reponse"
-            except Exception as e:
-                print str(e)
-                traceback.print_exc(file=sys.stdout)
-        else:
-            try:
-                print "**************session existe"
-                print  data['Devent']
-                print data['Dtypequery']
-                print "**************session existe"
-                if data['Devent'] ==  "packagesmissing" and data['Dtypequery'] == "TR":
-                    print  "packagesmissing"
-                    # il faut installer package sur machine.
-                    logging.getLogger().warn("package missing %s on machine %s"%(data['name'],data['jidmachine']))
-                    # transfert pacquage
-                    #transfert pacquage transfert le package sur machine et relance deploiement.
-                    datasend['data'] = data
-                    #supprimem os no used descriptor
-
-                    datasend['data']['Devent'] = "STARDEPLOY"
-                    datasend['data']['Dtypequery'] = "TQ"
-                    transfert_package(data['jidmachine'],datasend,objectxmpp)
-            except Exception as e:
-                traceback.print_exc(file=sys.stdout)
-        print "quit relayserver"
+            objectxmpp.session.createsessiondatainfo(sessionid,  datasession = datasend['data'], timevalid = 10)
+        logging.getLogger().debug("start call gracet")
+        grafcet(objectxmpp, datasend)
     else:
-        logging.getLogger().debug("#################MACHINE#####################")
-        logging.getLogger().debug("##############deploy %s on %s##############"%(data['name'],data['jidmachine'] ))
-        logging.getLogger().debug("#############################################")
-        # test package exist sur machine
-        # print "recois message"
-        # print json.dumps(data, indent=4, sort_keys=True)
-        if  'keyssh' in datasend['data']:
-            logging.getLogger().debug("Install key public RS")
-            installkeyssh(datasend['data']['keyssh'])
-            del datasend['data']['keyssh']
+        print "update session"
+        objectxmpp.session.sessionsetdata(sessionid, datasend) #save data in session
+        grafcet(objectxmpp, datasend)#grapcet va utiliser la session pour travaillé.
 
-        try:
-            if not 'stepcurrent' in datasend['data']:
 
-                if not 'sequence' in datasend['data']['descriptor'] and not checkosindescriptor(datasend['data']['descriptor']):
-                    print 'no checkosindescriptor'
-                    return 
 
-                datasend = cleandescriptor(datasend)
-                datasend['data']['pathpackageonmachine'] = os.path.join( managepackage.packagedir(),data['path'].split('/')[-1])
-
-                if managepackage.getpathpackagename(data['name']) is None:
-                    logging.getLogger().warn("packages %s missing on machine %s"%(data['name'], data['jidmachine']))
-                    datasend['data']['Devent'] = "packagesmissing"
-                    datasend['data']['Dtypequery'] = "TR"
-                    #namepackage = data['path'].split('/')[-1]
-                    #logging.getLogger().debug("SEND data to %s "%data['jidrelay'])
-                    logging.getLogger().debug("send envoi relayserver %s"% json.dumps(datasend, indent=4, sort_keys=True))
-                    objectxmpp.send_message(    mto=data['jidrelay'],
-                                                    mbody=json.dumps(datasend),
-                                                    mtype='chat')
-                else:
-                    if not 'stepcurrent' in datasend['data']:
-                        # data['stepcurrent'] est la tache qui va etre effectué.
-                        datasend['data']['stepcurrent'] = 0 #step initial
-                        logging.getLogger().debug("presence packages %s on machine %s"%(datasend['data']['name'], datasend['data']['jidmachine']))
-                        logging.getLogger().debug("start deploiement")
-                        # creation d'une session
-                        if not objectxmpp.session.isexist(sessionid):
-                            objectxmpp.session.createsessiondatainfo(sessionid,  datasession = datasend['data'], timevalid = 10)
-                            
-                        #logging.getLogger().info("%s"% json.dumps(datasend, indent=4, sort_keys=True))
-                        logging.getLogger().debug("start call gracet")
-                        grafcet(objectxmpp, datasend) # initialise graphcetgrapcet
-            else:
-                print "creation session"
-                objectxmpp.session.sessionsetdata(sessionid, datasend) #save data in session
-                print "grapcet"
-                grafcet(objectxmpp, datasend)#grapcet va utiliser la session pour travaillé.
-
-        except Exception as e:
-                traceback.print_exc(file=sys.stdout)
