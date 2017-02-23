4db19e1395c0f6c80680e791df4a20d4344ab5fe
SPO <stephane.pointu@siveo.net>
2017-02-23 14:53:24 +0100

    Inventory of Windows registry keys

--- pulse_agent_plugins/machine/plugin_inventory.py
+++ pulse_agent_plugins/machine/plugin_inventory.py
@@ -24,13 +24,16 @@ import sys, os
 from  lib.utils import simplecommand
 import zlib, base64
 import traceback
+import json
+if sys.platform.startswith('win'):
+    from lib.registerwindows import constantregisterwindows
+    import _winreg
 
-
-plugin={"VERSION": "1.0", "NAME" :"inventory", "TYPE":"machine"}
+plugin={"VERSION": "1.1", "NAME" :"inventory", "TYPE":"machine"}
 
 
 @pluginprocess
-def action( objetxmpp, action, sessionid, data, message, dataerreur, result):
+def action( xmppobject, action, sessionid, data, message, dataerreur, result):
     print "plugin_inventory"
     if sys.platform.startswith('linux'):
         try:
@@ -45,6 +48,7 @@ def action( objetxmpp, action, sessionid, data, message, dataerreur, result):
             raise
     elif sys.platform.startswith('win'):
         try:
+            # run the inventory
             program = os.path.join(os.environ["ProgramFiles"],'FusionInventory-Agent','fusioninventory-agent.bat')
             namefile = os.path.join(os.environ["ProgramFiles"], 'Pulse', 'tmp', 'inventory.txt')
             cmd = """\"%s\" --local=\"%s\""""%(program,namefile)
@@ -52,6 +56,31 @@ def action( objetxmpp, action, sessionid, data, message, dataerreur, result):
             Fichier = open(namefile,'r')
             result['data']['inventory'] = base64.b64encode(zlib.compress(Fichier.read(), 9))
             Fichier.close()
+            # read max_key_index parameter to find out the number of keys
+            if hasattr(xmppobject.config, 'max_key_index'):
+                result['data']['reginventory'] = {}
+                result['data']['reginventory']['info'] = {}
+                result['data']['reginventory']['info']['max_key_index'] = int(xmppobject.config.max_key_index)
+                nb_iter = int(xmppobject.config.max_key_index) + 1
+                # get the value of each key and create the json file
+                for num in range(1,nb_iter):
+                    reg_key_num = 'reg_key_'+str(num)
+                    result['data']['reginventory'][reg_key_num] = {}
+                    registry_key = getattr(xmppobject.config,reg_key_num)
+                    result['data']['reginventory'][reg_key_num]['key'] = registry_key
+                    hive = registry_key.split('\\')[0].strip('"')
+                    sub_key = registry_key.split('\\')[-1].strip('"')
+                    path = registry_key.replace(hive+'\\','').replace('\\'+sub_key,'').strip('"')
+                    print "hive: %s" % hive
+                    print "sub_key: %s" % sub_key
+                    print "path: %s" % path
+                    reg_constants = constantregisterwindows()
+                    key = _winreg.OpenKey(reg_constants.getkey(hive), path)
+                    (key_value, key_type) = _winreg.QueryValueEx(key,sub_key)
+                    result['data']['reginventory'][reg_key_num]['value'] = str(key_value)
+                    _winreg.CloseKey(key)
+                # generate the json and encode
+                result['data']['reginventory'] = base64.b64encode(json.dumps(result['data']['reginventory'], sort_keys=True, indent=4, separators=(',', ': ')))
         except Exception, e:
             print "Error: %s" % str(e)
             traceback.print_exc(file=sys.stdout)
