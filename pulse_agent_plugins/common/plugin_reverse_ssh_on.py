43366ae88830117591b1b8b0df64b4733e530833
Nicolas Lécureuil <neoclust@mageia.org>
2017-07-04 17:01:52 +0200

    Only one import by line

--- pulse_agent_plugins/common/plugin_reverse_ssh_on.py
+++ pulse_agent_plugins/common/plugin_reverse_ssh_on.py
@@ -19,7 +19,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-import sys, os
+import sys
+import os
 from subprocess import Popen
 import shlex
 import json
