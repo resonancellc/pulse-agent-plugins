6dc3d37879744d8c073ef879675478e3c00de495
spointu <spointu@gmail.com>
2018-08-06 14:50:48 +0200

    Version update

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
 
     keywords='pulse agent plugins',
     name='pulse_agent_plugins',
-    version='1.7',
+    version='1.8',
     debian_distro='stretch',
     description = 'XMPP Agent plugins for pulse',
     url='https://www.siveo.net/',
