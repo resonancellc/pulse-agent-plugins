4c4ef8febd1b95c692b5a9d4ed1c845ddf2e8a4b
Nicolas Lécureuil <neoclust@mageia.org>
2019-05-01 23:37:46 +0200

    Revert "Try to fix pylint error"
    This reverts commit 5c9073ee3a35bb727c1df7414b98725451fa7bc6.

--- pulse_agent_plugins/common/plugin_applicationdeploymentjson.py
+++ pulse_agent_plugins/common/plugin_applicationdeploymentjson.py
@@ -100,7 +100,7 @@ def changown_dir_of_file(dest, nameuser = None):
                                     "/setowner",
                                     nameuser,
                                     "/t"], stderr=STDOUT)
-        except CalledProcessError as e:
+        except subprocess.CalledProcessError as e:
             logger.error("%s changown_dir_of_file : %s"%(dest, str(e.output)))
 
 def cleandescriptor(datasend):
